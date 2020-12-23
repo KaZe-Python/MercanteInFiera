2edfccbedd90a1eb8b6eb4f7ac518683df5cb77f
KaZe-Python <bpcosentini239@gmail.com>
2020-12-23 18:47:15 +0100

    Any changes were made

--- static/js/server/handler.js
+++ static/js/server/handler.js
@@ -1,13 +1,13 @@
 window.addEventListener('load', function(){
     const buttonDeal = document.getElementsByName('deal');
     const buttonDealB = document.getElementsByName('dealb');
-    buttonDeal.addEventListener('click', async function(e){
+    buttonDeal.addEventListener('click',function(e){
         e.preventDefault();
-        await dealCardToPlayers();
+        dealCardToPlayers();
     });
-    buttonDealB.addEventListener('click', async function(e){
+    buttonDealB.addEventListener('click',function(e){
         e.preventDefault();
-        await dealBadCardToPlayers();
+        dealBadCardToPlayers();
     })
 
 });
